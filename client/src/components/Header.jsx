f8b2c8708a6bf6897301724a578a5b3d3261be4e
ChristianScarabelli <scachmabol@hotmail.it>
2025-01-23 11:18:51 +0100

    ADD: style

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,9 @@
 import logo from '../assets/logo.svg'
 export default function Header() {
     return (
-        <header>
+        <header className="p-4 flex justify-start items-center">
             <figure>
-                <img src={logo} alt='Bool BnB Logo' />
-                <figcaption>Book your perfect stay</figcaption>
+                <img className="h-40 w-auto" src={logo} alt='Bool BnB Logo' />
             </figure>
         </header>
     )
