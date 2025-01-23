8a445638d0dcee859dfdbf622910391268f90787
Vincenzo Iuliano <iuliano-vincenzo@live.it>
2025-01-23 11:30:23 +0100

    ADD: card layout

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,6 +1,6 @@
 export default function Card() {
     return <>
-        <section className="container max-w-fit px-2 py-2  flex flex-col bg-red-100">
+        <section className="container max-w-fit px-2 py-2  flex flex-col">
             <div>
                <img className="size-80 rounded" src="https://www.baitainmontagna.com/wp-content/uploads/2019/02/differenza-baita-chalet.jpeg" alt="" />
             </div>
