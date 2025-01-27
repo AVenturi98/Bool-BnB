c34d863b70ed14fd7d1ae48b8f42482441ed5dc4
ChristianScarabelli <scachmabol@hotmail.it>
2025-01-27 16:04:34 +0100

    ADD: infinite img carousel

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -2,7 +2,7 @@ import ImgCarousel from "./ImgCarousel"
 
 export default function Hero() {
     return (
-        <section className='bg-green-100 flex items-center justify-center my-10 p-10'>
+        <section className='bg-green-100 flex items-center justify-center my-5 p-10'>
             <ImgCarousel />
         </section>
     )
--- client/src/components/ImgCarousel.jsx
+++ client/src/components/ImgCarousel.jsx
@@ -11,10 +11,12 @@ import {
     CarouselPrevious,
 } from "@/components/ui/carousel"
 import { useState, useEffect } from "react"
+import placeHolder from '../assets/placeholder.png'
+import { Link } from "react-router"
 
 export default function ImgCarousel() {
     const plugin = React.useRef(
-        Autoplay({ delay: 2000, stopOnInteraction: false })
+        Autoplay({ stopOnInteraction: false })
     );
 
     const [properties, setProperties] = useState([])
@@ -34,18 +36,21 @@ export default function ImgCarousel() {
     }
 
     return (
-        <section className="p-4">
+        <section className="p-4 overflow-hidden ">
             <Carousel
                 plugins={[plugin.current]}
-                className="w-full max-w-screen-lg"
+                options={{ loop: true }}
+                className="w-full max-w-screen-lg "
             >
-                <CarouselContent className="-ml-2 md:-ml-4">
+                <CarouselContent className="-ml-2 md:-ml-4 animate-scroll">
                     {properties.map((property) => (
                         <CarouselItem key={property.id} className="pl-2 md:pl-4 sm:basis-1/2 md:basis-1/4">
-                            <div className="">
+                            <div>
                                 <Card className="h-full">
                                     <CardContent className="flex aspect-square items-center justify-center p-0">
-                                        <img src={property.img} alt={property.title} className="object-cover w-full h-full rounded-xl overflow-hidden" />
+                                        <Link to={`/api/properties/${property.id}`} className="block w-full h-full">
+                                            <img src={property.img || placeHolder} alt={property.title} className="object-cover w-full h-full rounded-xl overflow-hidden" />
+                                        </Link>
                                     </CardContent>
                                 </Card>
                             </div>
