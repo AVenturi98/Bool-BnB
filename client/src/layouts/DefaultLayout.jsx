478fdd9e3fe75a08cb424cf5780fea7bb44fc029
ChristianScarabelli <scachmabol@hotmail.it>
2025-01-27 12:34:12 +0100

    ADD: Hero and carousel component

--- client/src/layouts/DefaultLayout.jsx
+++ client/src/layouts/DefaultLayout.jsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router"
 import Header from "../components/Header"
 import Footer from "../components/Footer"
+import Hero from "@/components/Hero"
 
 export default function DefualtLayout() {
 
     return (
         <>
             <Header />
+            <Hero />
             <Outlet />
             <Footer />
         </>
