eec1207dbabfb36bc3fc35f95e854e93c4bc5f77
ChristianScarabelli <scachmabol@hotmail.it>
2025-01-28 11:39:26 +0100

    ADD: carousel hover, link from index to show, hearts in show

--- client/src/components/ImgCarousel.jsx
+++ client/src/components/ImgCarousel.jsx
@@ -1,4 +1,3 @@
-
 import * as React from "react"
 import axios from "axios"
 import Autoplay from "embla-carousel-autoplay"
@@ -42,14 +41,14 @@ export default function ImgCarousel() {
                 options={{ loop: true }}
                 className="w-full max-w-screen-lg "
             >
-                <CarouselContent className="-ml-2 md:-ml-4 animate-scroll">
+                <CarouselContent className="-ml-2 md:-ml-4 animate-scroll ">
                     {properties.map((property) => (
                         <CarouselItem key={property.id} className="pl-2 md:pl-4 sm:basis-1/2 md:basis-1/4">
                             <div>
-                                <Card className="h-full">
-                                    <CardContent className="flex aspect-square items-center justify-center p-0">
-                                        <Link to={`/${property.id}`} className="block w-full h-full">
-                                            <img src={property.img || placeHolder} alt={property.title} className="object-cover w-full h-full rounded-xl overflow-hidden" />
+                                <Card className="h-full overflow-hidden rounded-xl">
+                                    <CardContent className="flex w-full aspect-square items-center justify-center p-0">
+                                        <Link to={`/${property.id}`} className="block w-full h-full rounded-xl overflow-hidden">
+                                            <img src={property.img || placeHolder} alt={property.title} className="object-cover w-full h-full transition-transform duration-300 ease-in-out hover:scale-110" />
                                         </Link>
                                     </CardContent>
                                 </Card>
--- client/src/pages/Show.jsx
+++ client/src/pages/Show.jsx
@@ -1,15 +1,16 @@
+
+
 import { useState, useEffect } from "react"
 import { useParams } from "react-router"
 import axios from "axios"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faHeart } from "@fortawesome/free-regular-svg-icons"
 import { faSink, faPersonBooth, faBed, faWaveSquare } from "@fortawesome/free-solid-svg-icons"
 import greenLogo from '../assets/green.svg'
 import HeroShow from '../components/HeroShow'
 import MailForm from '../components/MailForm'
 import VoteStar from '../components/VoteStar'
 import defaultImg from '../assets/placeholder.png'
-import FormReview from "@/components/FormReview"
+import CounterButton from "@/components/Hearts"
 
 const text = `Lo Chalet Alpino, situato in Via delle Alpi 15 a Cortina, è una splendida proprietà che unisce eleganza e comfort in un contesto montano unico. Con una superficie di 200 m², la casa si sviluppa su due piani, offrendo ampi spazi per il relax e la convivialità. Composta da 6 stanze, tra cui 4 comodi letti e 3 bagni, è l'ideale per ospitare famiglie o gruppi di amici in cerca di una fuga nella natura senza rinunciare al lusso.
 L'interior design è pensato per creare un'atmosfera calda e accogliente, con elementi tipici dello stile alpino e finiture moderne. Le grandi finestre permettono di godere della vista panoramica sulle montagne circostanti, mentre i dettagli in legno e pietra conferiscono un tocco rustico e sofisticato.
@@ -21,6 +22,7 @@ export default function Show() {
     const [property, setProperty] = useState('')
     const [owner, setOwner] = useState('')
     const [review, setReview] = useState([])
+    const [hearts, setHearts] = useState(0)
 
     const { id } = useParams()
 
@@ -29,9 +31,8 @@ export default function Show() {
             .then(res => {
                 setProperty(res.data)
                 setOwner(res.data.owner[0])
-                console.log(res.data)
-
                 setReview(res.data.reviews)
+                setHearts(res.data.hearts)
                 console.log(res.data)
             })
             .catch(err => err.message)
@@ -39,13 +40,13 @@ export default function Show() {
 
     useEffect(() => {
         fetchPost()
-    }, [id, review.length])
+    }, [id])
 
     return (
         <div className="p-6 my-12 lg:px-60 border-b-4 border-grey-500">
             {/*  HERO  */}
             <section>
-                <HeroShow img={property.img ? property.img : defaultImg} room={property.rooms} bed={property.beds} bath={property.bathrooms} heart={property.hearts} title={property.title} />
+                <HeroShow img={property.img ? property.img : defaultImg} room={property.rooms} bed={property.beds} bath={property.bathrooms} heart={hearts} title={property.title} />
             </section>
             {/* DESCRIPTION */}
             <section className="mt-6">
@@ -55,10 +56,9 @@ export default function Show() {
                         <h4 className="text-2xl font-semibold">{property.building_type} a {property.city}, {property.address}</h4>
                     </div>
                     <div className="flex justify-between items-center gap-3 text-2xl">
-                        <div>{property.hearts}</div>
-                        <button type="button">
-                            <FontAwesomeIcon icon={faHeart} />
-                        </button>
+                        {property && property.hearts !== undefined && (
+                            <CounterButton property={property} />
+                        )}
                     </div>
                 </div>
                 <div className="m-6 text-lg">
@@ -91,18 +91,18 @@ export default function Show() {
                 <h1 className="text-2xl font-semibold mt-12">Hai bisogno di più informazioni?</h1>
                 <p className="my-3 text-lg">Mettiti in contatto con {owner.name}, il proprietario di casa</p>
                 <div className="text-lg my-3">{owner.name} :
-                    <span className="text-lg mx-24 italic "><button type="button" className="cursor-pointer hover:underline decoration-solid hover:text-green-700">{owner.owner_email}</button></span> </div>
-                <MailForm owner={owner.owner_email} />
+                    <span className="text-lg mx-24 italic "><button type="button" className="cursor-pointer hover:underline decoration-solid hover:text-green-700">{property.email}</button></span> </div>
+                <MailForm />
             </section>
             {/*  REVIEWS */}
             <section>
-                <h1 className="font-bold text-3xl mt-32 mb-6">RENCENSIONI</h1>
+                <h1 className="font-bold text-3xl mt-32 mb-6">RENCENSIONI ({review.length})</h1>
                 {review ?
                     review.map(element =>
                         <div key={element.id} className="py-12 flex justify-center items-center border-t-2 border-green-500">
 
                             <div className="relative isolate overflow-hidden bg-white">
-                                <div className="mx-auto max-w-2xl min-w-[500px] lg:max-w-4xl">
+                                <div className="mx-auto max-w-2xl lg:max-w-4xl">
                                     <div className="flex justify-around items-center">
                                         <div className="mt-8 flex items-center justify-center space-x-3 text-base">
                                             <div className="font-semibold text-gray-900 text-xl">{element.name}</div>
@@ -126,7 +126,8 @@ export default function Show() {
                     ) :
                     <div className="flex justify-center">Nessuna recensione</div>}
             </section>
-            <FormReview id={id} />
         </div>
     )
 }
+
+
