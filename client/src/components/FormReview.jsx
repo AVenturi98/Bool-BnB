a97f099c58ff7b4f9fc5bd64b55c09bb9e3056ab
MattiaEsposito98 <esposito.matty@hotmail.it>
2025-01-28 11:49:12 +0100

    add textarea

--- client/src/components/FormReview.jsx
+++ client/src/components/FormReview.jsx
@@ -98,12 +98,13 @@ export default function FormReview({ id }) {
                             Messaggio
                         </label>
                         <div className="mt-2">
-                            <input
+                            <textarea
                                 placeholder="Messaggio"
                                 value={review.text}
                                 name="text"
                                 onChange={handleChange}
                                 required
+                                maxLength={255}
                                 className="block w-full rounded-md bg-slate-100 px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-green-600 sm:text-sm/6"
 
                             />
