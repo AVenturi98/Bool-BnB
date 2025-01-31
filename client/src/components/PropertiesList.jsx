7f345b12f68c6f7097d3f7186d406e5e712a778b
AVenturi98 <lssndrventuri@gmail.com>
2025-01-31 16:06:35 +0100

    try component

--- client/src/components/PropertiesList.jsx
+++ client/src/components/PropertiesList.jsx
@@ -4,6 +4,8 @@ import Card from "./Card";
 import Hero from "./Hero.jsx";
 import GlobalContext from "../contexts/GlobalContext";
 
+import CarouselResponse from './CarouselResponse'
+
 export default function PropertiesList() {
   const [properties, setProperties] = useState([]);
   const [error, setError] = useState(null);
@@ -43,6 +45,7 @@ export default function PropertiesList() {
       <div>
         <Hero />
       </div>
+      <CarouselResponse />
       <div className="container mx-auto p-4">
         <h1 className="text-2xl font-bold text-green-700 mb-4">Immobili</h1>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
